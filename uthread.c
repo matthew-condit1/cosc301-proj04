2f511061012005ccc20058d0dbf2a9f7c38b5d48
Conor Oliver <coliver@colgate.edu>
2015-11-16 20:39:00 -0500

    finished thread create and join

--- uthread.c
+++ uthread.c
@@ -6,8 +6,16 @@
 
 
 //This is where you'll need to implement the user-level functions
- 
 
+#define NUMSTACK 64
+ 
+typedef struct	{
+	void * stack;
+	int pid;
+} processinfo
+
+int stackarr[NUMSTACK];
+int stackcount = 0;
 
 void lock_init(lock_t *lock) {
 	lock->locked = 1; //1 == locked 0 is unlocked
@@ -22,10 +30,35 @@ void lock_release(lock_t *lock) {
 }
 
 int thread_join(int pid) {
-    return -1;
+	int thread_pid = join(pid);
+		if(thread_pid < NUMSTACK && thread_pid > 0)
+		{
+			free(stackarr[thread_pid - 1].stack);
+		
+		}
+	
+	return thread_pid;
 }
 
 int thread_create(void (*start_routine)(void *), void *arg) {
-	return -1;
+	void * stack = malloc(PGSIZE * 2);
+	if((uint)stack % PGSIZE)
+	{
+		stack += PGSIZE - ((uint)stack % PGSIZE);
+	}
+	int thread_pid = clone(start_routine, arg, stack);
+	if(thread_pid = -1)
+	{
+		return -1:
+	}
+	
+	else
+	{
+		stackarr[stackcount].stack = stack;
+		stackarr[stackcount].pid = thread_pid;
+		stackcount++;
+	}
+	return thread_pid;
+	
 }
 
